e8a6a66cc515dfea01fc13676aa5811734f37363
Wootae01 <74524185+Wootae01@users.noreply.github.com>
2023-12-24 14:51:10 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -3,29 +3,14 @@
 using namespace std;
 
 int main(void) {
-	int tmp[7];
-	for (int i = 0; i < 7; i++) {
-		tmp[i] = 0;
-	}
-	int x, size = 0;
-	int stick = 64;
-	int res = -1;
+	int x;
 	scanf("%d", &x);
-	
-
-	while (size < 7) {
-		int sum = 0;
-		for (int i = 0; i < size; i++) {
-			sum += tmp[i];
-		}
-		sum += stick;
-
-		if (sum < x) {
-			tmp[size++] = stick;
-		}
-		else if (sum == x) {
-			res = size + 1;
-			break;
+	int res = 0;
+	int stick = 64;
+	while (stick >= 1) {
+		if (stick <= x) {
+			res++;
+			x -= stick;
 		}
 		stick /= 2;
 	}
