50ec6a09737b60dce3b255dbec4ca20b15509582
Wootae01 <74524185+Wootae01@users.noreply.github.com>
2024-03-03 23:23:01 +0900

    [Silver III] Title: N과 M (8), Time: 304 ms, Memory: 19652 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] res;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        res = new int[m];
+
+        Arrays.sort(num);
+        permutation(num, n ,m , 0);
+    }
+    static void permutation(int[] arr, int n, int m, int depth) throws IOException {
+        if (depth == m) {
+
+            for (int i = 0; i < m; i++) {
+                bw.write(res[i]+" ");
+            }
+            bw.write("\n"); bw.flush();
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if(depth > 0){
+                if (res[depth - 1] > arr[i]) {
+                    continue;
+                }
+            }
+            res[depth] = arr[i];
+            permutation(arr, n, m, depth+1);
+
+        }
+    }
+
+}
