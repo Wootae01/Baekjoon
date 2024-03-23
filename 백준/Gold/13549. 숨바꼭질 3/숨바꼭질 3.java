0fa69bb721fe5b63c1127a06c2902f7db69cfa15
Wootae01 <74524185+Wootae01@users.noreply.github.com>
2024-03-23 11:13:57 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 156 ms, Memory: 19112 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,58 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-public class Main {
-    static int K;
-    //소요되는 시간을 줄이기 위해서 visited 선언
-    static boolean[] visited;
-    static int min = Integer.MAX_VALUE;
-
-    public static void main(String[] args) throws IOException{
+class Main{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        visited = new boolean[100_001];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        bfs(N);
+        Queue<PosTime> queue = new LinkedList<>();
+        boolean[] visited = new boolean[100001];
 
+        queue.offer(new PosTime(n, 0));
+        visited[n] = true;
+
+        int min = Integer.MAX_VALUE;
+        while(!queue.isEmpty()){
+            PosTime current = queue.poll();
+            int sec = current.time;
+            int location = current.pos;
+
+            if (location == k) {
+                min = Math.min(min, sec);
+                continue;
+            }
+            if(location * 2 <= 100000 && !visited[location * 2]){
+                queue.offer(new PosTime(location  * 2, sec));
+                visited[location * 2] = true;
+            }
+            if(location-1 >= 0 && !visited[location-1]){
+                queue.offer(new PosTime(location - 1, sec + 1));
+                visited[location-1] = true;
+            }
+            
+            if(location + 1 <= 100000 && !visited[location + 1]){
+                queue.offer(new PosTime(location + 1, sec + 1));
+                visited[location + 1] = true;
+            }
+
+
+        }
         System.out.println(min);
+
     }
 
-    static void bfs(int val){
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(val, 0));
-        visited[val] = true;
+    static class PosTime{
+        int pos;
+        int time;
 
-        while(!q.isEmpty()){
-            Node a = q.poll();
-
-            if(a.x == K)
-                min = Math.min(min, a.time);
-
-            if(a.x*2<=100_000 && !visited[a.x*2]){
-                q.offer(new Node(a.x*2, a.time));
-                visited[a.x*2] = true;
-            }
-
-            if(a.x-1>=0 && !visited[a.x-1]){
-                q.offer(new Node(a.x-1, a.time + 1));
-                visited[a.x-1] = true;
-            }
-
-            if(a.x+1<=100_000 && !visited[a.x+1]){
-                q.offer(new Node(a.x+1, a.time + 1));
-                visited[a.x+1] = true;
-            }
+        PosTime(int pos, int time) {
+            this.pos = pos;
+            this.time = time;
         }
     }
 }
-class Node{
-    int x, time;
 
-    Node(int x, int time){
-        this.x = x;
-        this.time = time;
-    }
-}
