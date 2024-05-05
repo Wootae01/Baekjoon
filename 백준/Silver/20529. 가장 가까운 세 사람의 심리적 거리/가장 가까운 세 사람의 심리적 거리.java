b7b9282929af17e51f774e0944657d6118c9ee3a
Wootae01 <74524185+Wootae01@users.noreply.github.com>
2024-05-06 00:43:59 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 252 ms, Memory: 28016 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
@@ -3,52 +3,73 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
+class Main{
+    static int t;
+    static int n;
+    static String[] arr;
+    static boolean[] check;
+    static int res = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int N;
-        String[] mbtis;
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 0; t < T; t++) {
-            N = Integer.parseInt(br.readLine());
-            mbtis = new String[N];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                mbtis[i] = st.nextToken();
+        StringBuilder sb = new StringBuilder();
+
+        t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            if (n > 32) {
+                sb.append(0 + "\n");
+                continue;
             }
-            if(N > 32) {
-                System.out.println(0);
-            }else {
-                int result = 12; // 심리적 거리 최대 거리
-                for(int i = 0; i < N; i++) {
-                    for(int j = i + 1; j < N; j++) {
-                        for(int k = j + 1; k < N; k++) {
-                            result = Math.min(result,
-                                    distance(new String[]{mbtis[i], mbtis[j], mbtis[k]}));
-                        }
-                    }
+            arr = new String[n];
+            check = new boolean[n];
+
+            res = Integer.MAX_VALUE;
+
+            for (int j = 0; j < n; j++) {
+                arr[j] = st.nextToken();
+            }
+            dfs(0, 0);
+            sb.append(res + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int start, int depth) {
+        if (depth == 3) {
+            String[] select = new String[3];
+            int k = 0;
+            int distance = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (check[i]) {
+                    select[k++] = arr[i];
                 }
-                System.out.println(result);
+            }
+            for(int i = 0; i < 4; i++){
+                if(select[0].charAt(i) != select[1].charAt(i)){
+                    distance++;
+                }
+                if(select[1].charAt(i) != select[2].charAt(i)){
+                    distance++;
+                }
+                if(select[0].charAt(i) != select[2].charAt(i)){
+                    distance++;
+                }
             }
 
+            res = Math.min(res, distance);
+            return;
         }
 
-    }
+        for (int i = start; i < n; i++) {
 
-    /*
-     * 심리적 거리
-     */
-    public static int distance(String[] mbti) {
-        int dist = 0;
-        for(int i = 0; i < 4; i++) {
-            dist += mbti[0].charAt(i) != mbti[1].charAt(i)? 1:0;
-            dist += mbti[0].charAt(i) != mbti[2].charAt(i)? 1:0;
-            dist += mbti[1].charAt(i) != mbti[2].charAt(i)? 1:0;
+            if(!check[i]) {
+                check[i] = true;
+                dfs(i + 1, depth + 1);
+                check[i] = false;
+            }
         }
-
-        return dist;
     }
-
 }
