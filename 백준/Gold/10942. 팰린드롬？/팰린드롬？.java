bee8905e627abf1efaf802243b914e3b2547c6d1
Wootae01 <74524185+Wootae01@users.noreply.github.com>
2024-07-01 19:37:33 +0900

    [Gold IV] Title: 팰린드롬?, Time: 688 ms, Memory: 218004 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -3,11 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main{
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N+1];
+        int[] arr = new int[N + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
@@ -17,27 +17,43 @@ class Main{
         int M = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
+        // dp 계산
+        boolean[][] dp = new boolean[N + 1][N + 1];
+
+        // 길이가 1인 팰린드롬
+        for (int i = 1; i <= N; i++) {
+            dp[i][i] = true;
+        }
+
+        // 길이가 2인 팰린드롬
+        for (int i = 1; i < N; i++) {
+            if (arr[i] == arr[i + 1]) {
+                dp[i][i + 1] = true;
+            }
+        }
+
+        // 길이가 3 이상인 팰린드롬
+        for (int length = 3; length <= N; length++) {
+            for (int i = 1; i <= N - length + 1; i++) {
+                int j = i + length - 1;
+                if (arr[i] == arr[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        // 질의 처리
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
 
-            if (isValid(arr, S, E, (E - S) / 2)) {
+            if (dp[S][E]) {
                 sb.append("1\n");
-            }
-            else{
+            } else {
                 sb.append("0\n");
             }
         }
         System.out.println(sb);
-    }
-
-    static boolean isValid(int[] arr, int s, int e, int n) {
-        for (int i = 0; i <= n; i++) {
-            if (arr[s + i] != arr[e - i]) {
-                return false;
-            }
-        }
-        return true;
     }
 }
