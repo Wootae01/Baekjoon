3beec2e09fb58871d09c1e3ea076b31d2ad284eb
김우태 <kimwootae71@gmail.com>
2026-06-15 18:29:19 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+-- 아직 입양을 못 간 동물 중, 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회
+SELECT NAME, DATETIME AS DATETIME
+FROM (
+    SELECT I.NAME, I.DATETIME
+    FROM ANIMAL_INS I
+    LEFT JOIN ANIMAL_OUTS O
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+    WHERE O.DATETIME IS NULL
+    ORDER BY I.DATETIME
+)
+WHERE ROWNUM <= 3;
